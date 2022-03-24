20715017a6168a9a4602930544071b129f638677
Eric Moran <emoran0403@gmail.com>
2022-03-24 13:19:32 -0400

    added router and component files

---
+++ src/client/Navbar.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="d-flex justify-content-center mt-3">
+      <Link to="/" className="btn btn-primary nav-link mx-2 text-light">
+        Home
+      </Link>
+      <Link to="/pokedex" className="btn btn-primary nav-link mx-2 text-light">
+        Films
+      </Link>
+      <Link to="/boxes" className="btn btn-primary nav-link mx-2 text-light">
+        Characters
+      </Link>
+      <Link to="/milestones" className="btn btn-primary nav-link mx-2 text-light">
+        Locations
+      </Link>
+    </nav>
+  );
+};
+
+export default Navbar;
